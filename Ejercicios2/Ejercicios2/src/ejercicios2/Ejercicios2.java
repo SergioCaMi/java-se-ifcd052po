54214d445021fd2ffb752c8e7a9d0750ed2eeba7
Sergio Calvo <sergiocami84@gmail.com>
2026-02-02 10:16:23 +0100

    done

--- Ejercicios2/Ejercicios2/src/ejercicios2/Ejercicios2.java
+++ Ejercicios2/Ejercicios2/src/ejercicios2/Ejercicios2.java
@@ -13,16 +13,15 @@ public class Ejercicios2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // ej1();
-        //ej2();
-        //ej3();
-        //ej4();
-        mostrarTodasLasTablas();
-        //ej5();
-        //ej6();
-        //ej7();
-        //ej8();
-        //ej9();
+        ej1();
+        ej2();
+        ej3();
+        ej4();
+        ej5();
+        ej6();
+        ej7();
+        ej8();
+        ej9();
     }
 
     public static void ej1() {
@@ -68,7 +67,7 @@ o Cuántos días la temperatura fue superior a 25 grados.
         int mas25 = 0;
         String temperaturaSemanal = "";
         for (int t = 0; t < temperaturas.length; t++) {
-            temperaturas[t] = ThreadLocalRandom.current().nextInt(-20, 40 + 1);
+            temperaturas[t] = ThreadLocalRandom.current().nextDouble(10.0, 35.0);
             if (temperaturas[t] > 25) {
                 mas25++;
             }
@@ -125,12 +124,8 @@ Muestra las tablas de multiplicar del 1 al 10 en formato tabular utilizando prin
         String numeroUsuario = JOptionPane.showInputDialog("Introduce un número entre 1 y 10");
         int numero = Integer.parseInt(numeroUsuario);
         JOptionPane.showMessageDialog(null, mostrarTabla(numero));
+        mostrarTodasLasTablas();
 
-        String todasLasTablas = "";
-        for (int i = 1; i <= 10; i++) {
-            todasLasTablas += mostrarTabla(i) + "\n";
-        }
-        JOptionPane.showMessageDialog(null, todasLasTablas);
     }
 
     public static String mostrarTabla(int numero) {
@@ -141,29 +136,27 @@ Muestra las tablas de multiplicar del 1 al 10 en formato tabular utilizando prin
         return tabla;
     }
 
-public static void mostrarTodasLasTablas() {
+    public static void mostrarTodasLasTablas() {
         String tabla = "";
-            int i = 1;
+        int i = 1;
         for (int num = 1; num <= 10; num++) {
-                tabla += String.format("%-4d x %-4d = %-4d %-4d x %-4d = %-4d %-4d x %-4d = %-4d\n",
-                        i, num, (i * num),
-                        i + 1, num, ((i+1) * num),
-                        i + 2, num, ((i+2) * num),
-                        i + 3, num, ((i+3) * num),
-                        i + 4, num, ((i+4) * num),
-                        i + 5, num, ((i+5) * num),
-                        i + 6, num, ((i+6) * num),
-                        i + 7, num, ((i+7) * num),
-                        i + 8, num, ((i+8) * num),
-                        i + 9, num, ((i+9) * num),
-                        i + 10, num, ((i+10) * num)
-                        
-                );
-            }
-        
+            tabla += String.format("%02d x %02d = %02d | %02d x %02d = %02d | %02d x %02d = %02d | %02d x %02d = %02d | %02d x %02d = %02d | %02d x %02d = %02d | %02d x %02d = %02d | %02d x %02d = %02d | %02d x %02d = %02d | %02d x %02d = %02d | \n",
+                    i, num, (i * num),
+                    i + 1, num, ((i + 1) * num),
+                    i + 2, num, ((i + 2) * num),
+                    i + 3, num, ((i + 3) * num),
+                    i + 4, num, ((i + 4) * num),
+                    i + 5, num, ((i + 5) * num),
+                    i + 6, num, ((i + 6) * num),
+                    i + 7, num, ((i + 7) * num),
+                    i + 8, num, ((i + 8) * num),
+                    i + 9, num, ((i + 9) * num),
+                    i + 10, num, ((i + 10) * num)
+            );
+        }
+
         JOptionPane.showMessageDialog(null, tabla);
-    }  
-    
+    }
 
     public static void ej5() {
         System.out.println("-----------------------------------------");
@@ -177,13 +170,17 @@ Una universidad otorga becas según el promedio y la edad.
 o Beca de Excelencia: Promedio >= 9.0 y edad <= 25.
 o Beca de Apoyo: Promedio entre 7.5 y 8.9 (inclusive) o edad >=
 25 con promedio >= 8.0.
-o Sin Beca: Cualquier otro caso.
+o Sin Beca: Cualquier otro caso.J
 3. Usa estructuras if / else if / else para determinar qué beca le
 corresponde y muéstralo en un mensaje.      
          */
-        String inputEdad = JOptionPane.showInputDialog("Introduce tu promedio");
-        int edad = Integer.parseInt(inputEdad);
 
+        String inputPromedio = JOptionPane.showInputDialog("Introduce tu promedio");
+        float promedio = Float.parseFloat(inputPromedio);
+        String inputEdad = JOptionPane.showInputDialog("Introduce tu edad");
+        int edad = Integer.parseInt(inputEdad);
+        String mensaje = (promedio >= 9.0f && edad <= 25) ? "Beca de Excelencia" : (((promedio >= 7.5f && promedio <= 8.9f) || (promedio >= 8.0f && edad >= 25)) ? "Beca de Apoyo" : "Sin Beca");
+        JOptionPane.showMessageDialog(null, mensaje);
     }
 
     public static void ej6() {
@@ -205,6 +202,22 @@ Pista:
 Necesitarás una variable boolean encontrada que cambie a true si hay
 coincidencia.        
          */
+        String[] inventario = {"Espada", "Escudo", "Poción", "Capa", "Anillo"};
+        String objetos = "";
+        for (String obj : inventario) {
+            objetos += obj + ", ";
+        }
+        String inputNombre = JOptionPane.showInputDialog("Introduce el objeto a buscar: " + objetos);
+        for (int i = 0; i < inventario.length; i++) {
+            System.out.println(inventario[i].toLowerCase() + " - " + inputNombre.toLowerCase());
+
+            if (inputNombre.toLowerCase().equals(inventario[i].toLowerCase())) {
+                JOptionPane.showMessageDialog(null, "El objeto " + inventario[i] + " está en el hueco " + i + ".");
+
+            }
+
+        }
+
     }
 
     public static void ej7() {
@@ -222,6 +235,20 @@ o El monstruo quita al héroe un número aleatorio entre 7 y 12.
 3. Al final, usa un if para anunciar quién ganó o si hubo un empate
 técnico (ambos mueren a la vez).       
          */
+        int vidaMonstruo = 100;
+        int vidaHeroe = 100;
+        String mensaje = "";
+        do {
+            vidaMonstruo -= ThreadLocalRandom.current().nextInt(5, 15 + 1);
+            vidaHeroe -= ThreadLocalRandom.current().nextInt(7, 12 + 1);
+            System.out.println("Vida Monstruo: " + vidaMonstruo);
+            System.out.println("Vida Héroe: " + vidaHeroe);
+        } while (vidaMonstruo > 0 && vidaHeroe > 0);
+        mensaje = (vidaMonstruo > vidaHeroe) ? "El Monstruo ha ganado..." : "El Héroe ha ganado!!";
+        if (vidaMonstruo == vidaHeroe) {
+            mensaje = "Empate técnico! Ambos han muerto en la batalla.";
+        }
+        JOptionPane.showMessageDialog(null, mensaje);
     }
 
     public static void ej8() {
@@ -239,6 +266,26 @@ validar la entrada antes de pasar al siguiente índice).
 3. Al finalizar, calcula el total de la compra y aplícale un descuento del
 10% si el total supera los 100€.        
          */
+
+        double[] precios = new double[5];
+        int index = 0;
+        int suma = 0;
+        do {
+            String inputPrecio = JOptionPane.showInputDialog("Introduce un prcio:");
+            precios[index] = Double.parseDouble(inputPrecio);
+            if (precios[index] < 0) {
+                JOptionPane.showMessageDialog(null, "Vuelve a introducir el precio. No son validos valores negativos");
+            } else {
+                suma += precios[index];
+                index++;
+            }
+        } while (index < precios.length);
+        if (suma > 100) {
+            JOptionPane.showMessageDialog(null, "La suma total es: " + suma + "€. Con el 10% de descuento: " + (suma - (suma * 0.1)) + "€.");
+        } else {
+            JOptionPane.showMessageDialog(null, "La suma total es: " + suma + ".");
+        }
+
     }
 
     public static void ej9() {
@@ -256,6 +303,24 @@ o Cuenta cuántos críticos hubo en total.
 o Suma sólo el daño de los ataques que no fueron críticos.
 3. Muestra el resumen final al usuario.
          */
+        int numeros[] = new int[10];
+        int criticos = 0;
+        int noCriticos = 0;
+        for (int t = 0; t < numeros.length; t++) {
+            numeros[t] = ThreadLocalRandom.current().nextInt(1, 101);
+            if (numeros[t]>85){
+            criticos++;
+            } else {
+            noCriticos += numeros[t];
+            } 
+            System.out.println(numeros[t]);
+            
+            
+        }
+        System.out.println("Cantidad de daños críticos: "+criticos);
+            System.out.println("Cantidad de daño total no crítico: "+noCriticos);
+        
+
     }
 
 }
